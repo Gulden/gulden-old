11fd53d1d195e1a0abcca4460606f85ae44c9161
GeertJohan <gjr19912@gmail.com>
2014-09-25 16:13:05 +0200

    s/GuldenCoin/Guldencoin for CLIENT_NAME

--- src/version.cpp
+++ src/version.cpp
@@ -6,9 +6,9 @@
 #include "version.h"
 
 // Name of client reported in the 'version' message. Report the same name
-// for both bitcoind and bitcoin-qt, to make it harder for attackers to
+// for both guldencoind and guldencoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("GuldenCoin");
+const std::string CLIENT_NAME("Guldencoin");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
